40adc1b09a3ad544c10271b2b063cee79a836e97
Jerry Lin <linj1064@gmail.com>
2020-12-09 22:04:09 -0800

    setup Github Actions workflow for AWS ECS CI/CD

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM node:latest
 ENV NODE_ENV=production
+ARG DISCORD_BOT_TOKEN
+ENV DISCORD_BOT_TOKEN=${DISCORD_BOT_TOKEN}
 WORKDIR /bot
 COPY ["package.json", "yarn.lock", "./"]
 RUN yarn
--- src/commands/wish.js
+++ src/commands/wish.js
@@ -2,6 +2,6 @@ module.exports = {
   name: 'wish',
   description: 'Wishing simulator.  Wishes are of unknown vintage.',
   execute(message, args) {
-    message.channel.send('Wish simulator has yet to be implemented. By the way, does ECS CI/CD work?');
+    message.channel.send('Wish simulator has yet to be implemented.');
   },
 };
